7e367ed1b3096cf36e45349e2cfad7992c0cb38a
Natalia Chigryay <123581931+nchigryay@users.noreply.github.com>
2025-05-12 18:42:48 +0300

    Setup t3-oss (#6)
    * Setup t3-oss
    
    * Update README

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,14 +1,4 @@
-export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-04-17'
-
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-)
-
-export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
-)
+import { env } from '../../env.mjs'
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
@@ -17,3 +7,15 @@ function assertValue<T>(v: T | undefined, errorMessage: string): T {
 
   return v
 }
+
+export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-04-17'
+
+export const dataset = assertValue(
+  env.NEXT_PUBLIC_SANITY_DATASET,
+  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
+)
+
+export const projectId = assertValue(
+  env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+)
