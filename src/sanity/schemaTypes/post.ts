041c426cfbcbf8c605d30d3c56cd0ff2e1a7681b
Natalia Chigryay <123581931+nchigryay@users.noreply.github.com>
2025-05-16 19:50:05 +0300

    Fix language field import (#9)

--- src/sanity/schemaTypes/post.ts
+++ src/sanity/schemaTypes/post.ts
@@ -1,5 +1,5 @@
+import { LANGUAGE_FIELD } from '@solana/ms-tools-integrations'
 import { defineField, defineType } from 'sanity'
-import { LANGUAGE_FIELD } from '../../constants/language'
 
 export const post = defineType({
   name: 'post',
