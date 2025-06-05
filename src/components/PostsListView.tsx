d2a3f4dfbd2aad96177fe7d9d0b68654d68e1e0d
nchigryay <natalia.chigryay@hoodies.team>
2025-06-05 22:23:49 +0100

    Add install script

--- src/components/PostsListView.tsx
+++ src/components/PostsListView.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
-import { Link } from '@/i18n/navigation'
 import { type SanityDocument } from 'next-sanity'
+import { Link } from '@/i18n/navigation'
 import { urlFor } from '@/sanity/lib/image'
 
 export interface IPostsListViewProps {
