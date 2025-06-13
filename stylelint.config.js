42e0da65fdb1432183f5fcb17a1f057abb91e4f3
Natalia Chigryay <123581931+nchigryay@users.noreply.github.com>
2025-06-13 20:29:55 +0100

    Remove Stylelint (#12)
    * Remove Stylelint
    
    * Add app name from env to jsonld

--- stylelint.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('stylelint').Config} */
-const config = {
-  extends: ['@solana-foundation/ms-tools-config/stylelint.config.js'],
-  // Add any UI-specific style rules here
-}
-
-export default config
